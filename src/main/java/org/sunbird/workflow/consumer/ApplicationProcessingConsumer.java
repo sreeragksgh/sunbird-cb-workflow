1d5c19cdb1edeee39b3e070be5d68a407ab82c9d
sreeragksgh <sreeragsajeesh@gmail.com>
2023-03-31 17:17:33 +0530

    fix for log duplication

--- src/main/java/org/sunbird/workflow/consumer/ApplicationProcessingConsumer.java
+++ src/main/java/org/sunbird/workflow/consumer/ApplicationProcessingConsumer.java
@@ -28,7 +28,10 @@ public class ApplicationProcessingConsumer {
     private WorkflowAuditProcessingServiceImpl workflowAuditProcessingService;
 
 
-    @KafkaListener(id = "id0", groupId = "workflowContentTopic-consumer", topicPartitions = {@TopicPartition(topic = "${kafka.topics.workflow.request}", partitions = {"0"})})
+
+
+//    @KafkaListener(id = "id0", groupId = "workflowContentTopic-consumer", topicPartitions = {@TopicPartition(topic = "${kafka.topics.workflow.request}", partitions = {"0"})})
+    @KafkaListener(id = "id0", topics = "${kafka.topics.workflow.request}", groupId = "workflowContentTopic-consumer")
     public void processMessage(ConsumerRecord<String, String> data) {
         WfRequest wfRequest = null;
         try {
